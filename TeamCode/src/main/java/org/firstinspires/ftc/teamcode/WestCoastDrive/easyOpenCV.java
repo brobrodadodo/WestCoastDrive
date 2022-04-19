8b0c96bde1596911b4b38adf14323ca97df91542
brobrodadodo <comicbros94@gmail.com>
2022-04-19 18:49:38 -0400

    swag money

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WestCoastDrive/easyOpenCV.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WestCoastDrive/easyOpenCV.java
@@ -2,3 +2,22 @@ package org.firstinspires.ftc.teamcode.WestCoastDrive;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+@TeleOp
+
+class openCvTest extends OpenCvPipeline {
+    @Override
+    public Mat processFrame(Mat input) {
+        return input;
+    }
+}

8ab34374198398b41e3d8fcbd47a534990beacf0
andrewnoob <23665583+andrewnoob000@users.noreply.github.com>
2022-04-23 14:35:18 -0400

    op mode for testing gimbal

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WestCoastDrive/GimbalTest.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.WestCoastDrive;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp (name = "GimbalTest", group = "Test")
+public class GimbalTest extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        Servo yawServo = hardwareMap.servo.get("webcamYaw");
+        Servo pitchServo = hardwareMap.servo.get("webcamPitch");
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while(opModeIsActive()) {
+            double left = (-gamepad1.left_stick_y + 1) * 0.5;
+            double right = (gamepad1.right_stick_y + 1) * 0.5;
+
+            yawServo.setPosition(left);
+            pitchServo.setPosition(right);
+        }
+    }
+}
